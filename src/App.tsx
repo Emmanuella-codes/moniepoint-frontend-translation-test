eac937abcf856d39f6757652de31efc63e152d21
Emmanuella <emmanuella.chukwu77@gmail.com>
2023-09-15 12:48:35 +0100

    create navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-3xl bg-red-700">some code</p>
+    <div className="">
+      <header className="">
+        <Navbar />
       </header>
     </div>
   );
