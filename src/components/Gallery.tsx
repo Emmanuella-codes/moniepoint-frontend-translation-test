253c2b3c4b6db4fe07d4655a3c154c7ecd0a5302
Emmanuella <emmanuella.chukwu77@gmail.com>
2023-09-16 15:51:10 +0100

    update gallery component / minor fix on hero desktop view

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -2,33 +2,71 @@ import React from "react";
 
 const Gallery = () => {
   return (
-    <div className="text-white">
-      <div className="grid grid-col-3 gap-2">
-        <div className="col-span-3 bg-gallery-pic-1">
-          <button>Home insurance</button>
-        </div>
-        <div className="col-span-1"></div>
-        <div className="col-span-2 p-4">
-          <h4 className="font-primary-1">Best choice</h4>
-          <p>
-            Not all insurance are the same. That's why agency knows that your
-            insurance should be customized to fit your situation.
-          </p>
-          <button className="bg-mid-orange px-8 py-1 rounded-full text-black">
-            View all services
+    <div className="text-white my-16 px-[5rem]">
+      <div className="grid grid-cols-5 gap-12">
+        <div className="gallery-1 col-span-2 relative">
+          <img
+            src="/images/living-interior.jpg"
+            alt="interior"
+            className="rounded-3xl"
+          />
+          <button className="absolute bottom-10 left-3 bg-black rounded-full px-5 py-2">
+            Home insurance
           </button>
+        </div>
+        <div className="col-span-1">
+          <img
+            src="/images/lifebuoy.jpg"
+            alt="lifebuoy"
+            className="rounded-full h-[55vh]"
+          />
+        </div>
+        <div className="col-span-2 px-14 py-9 bg-dark-blue rounded-[4rem]">
+          <div>
+            <h4 className="font-primary-1 text-[4rem] pb-4">Best choice</h4>
+            <p className="pb-10">
+              Not all insurance are the same. That's why agency knows that your
+              insurance should be customized to fit your situation.
+            </p>
+          </div>
+          <div className="pt-[5rem]">
+            <button className="bg-mid-orange px-9 py-3 rounded-full text-black">
+              View all services
+            </button>
+          </div>
         </div>
       </div>
       {/* same size images */}
-      <div className="grid grid-cols-3">
-        <div>
-          <button>Vehicle damage</button>
+      <div className="grid grid-cols-3 gap-4 pt-14">
+        <div className="relative">
+          <img
+            src="/images/car.jpg"
+            alt="car"
+            className="rounded-2xl h-[40vh]"
+          />
+          <button className="absolute bottom-4 left-3 bg-black rounded-full px-5 py-2">
+            Vehicle damage
+          </button>
         </div>
-        <div>
-          <button>Mortgage insurance</button>
+        <div className="relative">
+          <img
+            src="/images/cottage.jpg"
+            alt="house"
+            className="rounded-2xl h-[40vh]"
+          />
+          <button className="absolute bottom-4 left-3 bg-black rounded-full px-5 py-2">
+            Mortgage insurance
+          </button>
         </div>
-        <div>
-          <button>Medical expenses</button>
+        <div className="relative">
+          <img
+            src="/images/pills.jpg"
+            alt="pills"
+            className="rounded-2xl h-[40vh]"
+          />
+          <button className="absolute bottom-4 left-3 bg-black rounded-full px-5 py-2">
+            Medical expenses
+          </button>
         </div>
       </div>
     </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import { HiOutlineArrowLongDown } from "react-icons/hi2";
 
 const Hero = () => {
   return (
-    <div className="grid grid-cols-2 gap-2 px-10 py-5">
+    <div className="grid grid-cols-2 gap-2 px-20 py-5">
       {/* hero elements */}
       <div>
         <h1 className="font-primary-1 text-white text-[9.4rem] leading-[0.9] font-bold">
